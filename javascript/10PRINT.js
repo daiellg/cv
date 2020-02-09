91bf54e9294f079474e13f24f688ca67a03e3833
Daniel Gąsiorek <gasiorek.d@gmail.com>
2020-02-09 18:06:24 +0100

    Color and spacing refine

--- javascript/10PRINT.js
+++ javascript/10PRINT.js
@@ -1,6 +1,6 @@
 let x = 0;
 let y = 0;
-let spacing = 20;
+let spacing = 25;
 var canvas;
 
 // function windowResized() {
@@ -11,13 +11,13 @@ function setup() {
     canvas = createCanvas(windowWidth, windowHeight);
     canvas.position(0, 0);
     canvas.style('z-index', '-1');
-    background(255);
+    background('#FFFAFA');
     
 }
 
 function draw() {
-    stroke(0);
-    strokeWeight(3)
+    stroke('#333333');
+    strokeWeight(3);
     if (random(1) < 0.5) {
     line(x, y, x + spacing, y + spacing);
 }    else {
