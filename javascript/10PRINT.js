0ca764a7f6fc09403c42a3265acd7cbadd49ae8c
Daniel Gąsiorek <gasiorek.d@gmail.com>
2020-02-09 16:54:32 +0100

    daiellg

--- javascript/10PRINT.js
+++ javascript/10PRINT.js
@@ -1,15 +1,23 @@
 let x = 0;
 let y = 0;
-let spacing = 10;
+let spacing = 20;
+var canvas;
+
+// function windowResized() {
+//     resizeCanvas(windowWidth, windowHeight);
+// }
 
 function setup() {
-    createCanvas(400, 400);
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.position(0, 0);
+    canvas.style('z-index', '-1');
     background(255);
-    frameRate(1000);
+    
 }
 
 function draw() {
     stroke(0);
+    strokeWeight(3)
     if (random(1) < 0.5) {
     line(x, y, x + spacing, y + spacing);
 }    else {
@@ -20,4 +28,4 @@ function draw() {
         x = 0;
         y = y + spacing;
     }
-}
+} 
